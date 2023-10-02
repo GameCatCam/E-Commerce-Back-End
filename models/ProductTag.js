00bc1242efec85ec4617b6f2237040d14146b466
GameCatCam <cameronrawlins11@gmail.com>
2023-10-02 17:25:14 -0400

    Created sequelize models and associations

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const { databaseVersion } = require('../../../../../CWRU-VIRT-FSF-PT-06-2023-U-LOLC/13-ORM/01-Activities/14-Stu_Validation/Solved/config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: id,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: id,
+      },
+    },
   },
   {
     sequelize,
